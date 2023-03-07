b6c4a8c5a2bc03b1aa048431772b7662b271fc7d
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-03-07 17:37:05 +0100

    feat: :sparkles: Adds complete exercise, with comment about optional way to solve exercise

--- js/script.js
+++ js/script.js
@@ -6,21 +6,24 @@ console.log(userName, typeof userName);
 
 /* Chiedere all'utente il suo cognome */
 
-const userSurname = prompt("Ciao Utente, qual è il tuo cognome?")
+const userSurname = prompt(`Ciao ${userName}, qual è il tuo cognome?`)
 console.log(userSurname, typeof userSurname);
 
 /* Chiedere all'utente il suo colore preferito */
 
-const userFavColor = prompt("Ciao Utente, qual è il tuo colore preferito?")
+const userFavColor = prompt(`Ciao ${userName} ${userSurname}, qual è il tuo colore preferito?`)
 console.log(userFavColor, typeof userFavColor);
 
 //Unire le risposte
 
-const result = userName + " " + userSurname + " " + userFavColor
+//const result = userName + " " + userSurname + " " + userFavColor;
+/* ^ opzionale ^ */
+const result = `La tua password è: ${userName}${userSurname}${userFavColor}21`
 console.log("Tutte Le Risposte", result);
 
 //OUTPUT
 //Stampare sulla pagina le risposte unite
 
 document.getElementById("user-result").innerHTML 
-= 'Informazioni Utente: ${result}';
+= `Informazioni Utente: ${result}`;
+
